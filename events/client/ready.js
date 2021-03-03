37e1647ea0eeee16b0b885333ffaed7347e1cc4f
AKIR4 <68711471+Ak1r-4@users.noreply.github.com>
2021-03-03 09:16:39 +0100

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -9,22 +9,5 @@ module.exports = async client => {
 
     console.log(chalk.red`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Active, Commands Loaded!`);
     console.log(chalk.red`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} Logged In!`);
-      client.on('ready', () => { //sasuke
-	function randomStatus() {//sasuke
-		let status = [//sasuke
-			PREFIX + 'help',
-			`School is Back :(`,
-      `anime`
-		];
-		let rstatus = Math.floor(Math.random() * status.length);
-
-		client.user.setActivity(status[rstatus], {
-			type: 'PLAYING', status: "dnd" 
-		});
-	}
-	setInterval(randomStatus, 2000);
-
-	console.log(`Hi, ${client.user.username} is now online!`);
-});
     console.log(chalk.blue`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Now ` + totalChannels + ` channels, ` + totalGuilds + ` Servers and ` + totalUsers + ` serving  users!`);
 }
