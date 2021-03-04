3172a15380f5c607431c3cc66215b3420ab5ae12
AKIR4 <68711471+Ak1r-4@users.noreply.github.com>
2021-03-04 23:13:36 +0100

    Create gay.js

---
+++ commands/image/gay.js
@@ -0,0 +1,24 @@
+const {MessageAttachment} = require('discord.js');
+const Discord = require("discord.js");
+const config = require('../../configs/config.json');
+const emotes = require('../../configs/emotes.json')
+const AmeClient = require('amethyste-api');
+const AmeAPI = new AmeClient(config.ameAPI);
+module.exports = {
+    config: {
+        name: 'gay',
+        description: 'colors your avatar to rainbow flag',
+        aliases: ["gay"],
+        usage: '',
+        accessableby: "",
+    },
+    run: async (client, message, args) => {
+    let user = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member;
+        let m = await message.channel.send("**Please Wait...**");
+        let buffer = await AmeAPI.generate("gay", { url: user.user.displayAvatarURL({ format: "png", size: 1024 }) });
+        let attachment = new Discord.MessageAttachment(buffer, "gay.png");
+        m.delete({ timeout: 2000 });
+        message.channel.send(attachment);
+
+    }
+}
