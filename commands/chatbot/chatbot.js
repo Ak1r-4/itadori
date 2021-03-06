cee563418cde8edfb18b96791210c1b1828c997a
AKIR4 <68711471+Ak1r-4@users.noreply.github.com>
2021-03-06 18:40:47 +0100

    Update chatbot.js

--- commands/chatbot/chatbot.js
+++ commands/chatbot/chatbot.js
@@ -43,8 +43,8 @@ module.exports = {
       )
       .setTimestamp()
   .setFooter(
-      "© Kakashi",
-      "https://cdn.discordapp.com/avatars/807734261901820004/03d30e04f9c3e1ecb7a865d3cb7c859c.webp?size=1024"
+      "© Itadori",
+      "https://media.discordapp.net/attachments/815004467979223040/817813789382410270/0e8ea2cf9e9a50a87464380a8d083295.jpg?width=676&height=676"
     )
       .setColor(config.embedcolor);
     
@@ -79,9 +79,9 @@ module.exports = {
         true
       )
       .setTimestamp()
-.setFooter(
-      "© Kakashi",
-      "https://cdn.discordapp.com/avatars/807734261901820004/03d30e04f9c3e1ecb7a865d3cb7c859c.webp?size=1024"
+  .setFooter(
+      "© Itadori",
+      "https://media.discordapp.net/attachments/815004467979223040/817813789382410270/0e8ea2cf9e9a50a87464380a8d083295.jpg?width=676&height=676"
     )
       .setColor(config.embedcolor);
     message.channel.send(embed);
