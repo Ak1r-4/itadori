c4b58b6d75d0256c37ce79c05e4dc5ad237ab9a5
AKIR4 <68711471+Ak1r-4@users.noreply.github.com>
2021-03-07 03:15:07 +0100

    Update invite.js

--- commands/info/invite.js
+++ commands/info/invite.js
@@ -15,7 +15,7 @@ module.exports = {
 const embed = new Discord.MessageEmbed()
         .setThumbnail(client.user.avatarURL())
     .setDescription(
-      "**Kakashi Bot ** \n\n**👋 Hey!\n Do you want Invite me? [Click Here](https://discord.com/oauth2/authorize?client_id=807734261901820004&scope=bot&permissions=1412955263) to Invite me!\nThanks for supporting me.** <a:Heart:813359786782883851> "
+      "**Itadori** \n\n**👋 Hey!\n Do you want Invite me? [Click Here](https://discord.com/oauth2/authorize?client_id=807734261901820004&scope=bot&permissions=1412955263) to Invite me!\nThanks for supporting me.** <a:Heart:813359786782883851> "
     )
     .addField("**Server Support**", "**[Click Here](https://discord.gg/NQtZNcbVBc)**", true)
     .addField("**Vote**", "**Soon !**", true)
