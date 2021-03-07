17f79a357bff3b71cd45b127d0354f3170352c68
AKIR4 <68711471+Ak1r-4@users.noreply.github.com>
2021-03-07 03:26:00 +0100

    Update command.js

--- handlers/command.js
+++ handlers/command.js
@@ -10,5 +10,5 @@ module.exports = (client) => {
             if (pull.config.aliases) pull.config.aliases.forEach(cmd => client.aliases.set(cmd, pull.config.name));
         };
     };
-    ["info", "anime", "nsfw", "image", "chatbot", "fun", "general", "animals"].forEach(cmd => load(cmd));
+    ["animals", "info", "anime", "nsfw", "image", "chatbot", "fun", "general"].forEach(cmd => load(cmd));
 };
