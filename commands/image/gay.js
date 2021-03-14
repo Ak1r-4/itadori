3e744fcca0a9344ffb51b64849b21b2fda5f71d1
AKIR4 <68711471+Ak1r-4@users.noreply.github.com>
2021-03-14 11:21:46 +0100

    Update gay.js

--- commands/image/gay.js
+++ commands/image/gay.js
@@ -15,14 +15,10 @@ module.exports = {
     run: async (client, message, args) => {
          let user = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member;
         let m = await message.channel.send("**Please Wait...**");
-        let buffer = await ameAPI.generate("gay", { url: user.user.displayAvatarURL({format: "gif", size: 2048 }) });
-        let attachment = new Discord.MessageAttachment(buffer, "gay.gif");
+        let buffer = await ameAPI.generate("gay", { url: user.user.displayAvatarURL({ format: "png", size: 1024 }) });
+        let attachment = new Discord.MessageAttachment(buffer, "gay.png");
         m.delete({ timeout: 1000 });
-        const embed = new Discord.MessageEmbed()
-        .setImage(attachment)
-        .setTimestamp()
-        .setColor(config.embedcolor);
-        message.channel.send(embed);
+        message.channel.send(attachment);
 
   }
 }
